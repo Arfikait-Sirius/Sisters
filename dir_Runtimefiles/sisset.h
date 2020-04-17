efe907a97c5f8bf413da5ea29dee46fbcde7201b
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2020-04-17 18:15:14 +0900

    Create Sisters settings.

---
+++ dir_Runtimefiles/sisset.h
@@ -0,0 +1,28 @@
+#ifndef _SISSET_
+#define _SISSET_
+
+
+// [ COMMON ]
+#define LOG_SETUP 1
+
+
+// Dorothy
+#define MAX_FILE_COUNT 16
+#define READ_BUFSIZE 1024
+
+
+// Louise
+#define MAX_CPY_SIZE 512
+
+
+// Kotone
+#define MAX_LIST_COUNT 16
+#define MAX_LIST_SIZE 128
+
+
+// Sylvia
+#define OPERATOR_STACK_BUFSIZE 64
+#define NUMBER_STACK_BUFSIZE 64
+
+
+#endif
