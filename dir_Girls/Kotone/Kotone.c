b3332c7d75680de7e0b9eb60d1288c2d556acc96
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2020-06-13 22:37:52 +0900

    Retake Kotone.

--- dir_Girls/Kotone/Kotone.c
+++ dir_Girls/Kotone/Kotone.c
@@ -1,154 +1,90 @@
 #include "Kotone.h"
 
+// Name
+static string MyName = "Kotone" ;
+
+// Kotone's Memo
 static _MyDataKotone MyData = INIT_KOTONE_MYDATA ;
 
-int mfnKotone( int argc, char* argv[] ){
-     return CHAO ;
+// Birth
+KotoneFunctions callKotone( void ){
+     KotoneFunctions _KotoneSkills = BIRTH_KOTONE ;
+
+     return _KotoneSkills ;
 }
 
-int _fnCreateList( char type, char mode ){
-     int seq = MyData.listSeq ;
+//------------------------
+// :[ NAME ]:
+//     fnCreate
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+int _fnCreateList( mode m ){
 
-     MyData.existList[seq] = TYPE_DFT ;
-     switch( type ){
-     case TYPE_PTR:
-          MyData.existList[seq] = TYPE_PTR ;
-          break ;
-     default:
-          ;
+     if( m == TMP ){
+          return TMP_LIST ;
      }
-	switch( mode ){
-	case TMP:
-          MyData.chrListIndex[seq] = LIST_TOP ;
-          MyData.strListIndex[seq] = LIST_TOP ;
-          MyData.intListIndex[seq] = LIST_TOP ;
-          MyData.PListIndex[seq] = LIST_TOP ;
-          MyData.PListIndexAccess[seq] = LIST_TOP ;
-		break ;
-	case FIX:
-		MyData.listSeq++ ;
-		break ;
-	}
 
-	logSisters(
-			"fnCreateList()",
-			ATTEND,
-			Kotone_ID
-			) ;
-
-     return seq ;
+     return MyData.seq++ ;
 }
 
-void _fnAppendPList( int listID, void* param, char type ){
+//------------------------
+// :[ NAME ]:
+//     fnAdd_p
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+void _fnAddList_p( int listID, pointer p ){
 
-     if( _isNotExistList( listID, TYPE_PTR ) ){
-          logSisters(
-                    "\"PList\" is not created.",
-                    FATAL,
-                    Kotone_ID
-                    ) ;
-          return ;
-     }
-
-     if( type == TYPE_CHR ){
-          MyData.chrList[listID][MyData.chrListIndex[listID]] = *( char* )param ;
-          MyData.PList[listID][MyData.PListIndex[listID]] = MyData.chrList[listID] + MyData.chrListIndex[listID]++ ;
-     }else if( type == TYPE_STR ){
-          strcpy( MyData.strList[listID][MyData.strListIndex[listID]], ( char* )param ) ;
-          MyData.PList[listID][MyData.PListIndex[listID]] = MyData.strList[listID] + MyData.strListIndex[listID]++ ;
-     }else if( type == TYPE_INT ){
-          MyData.intList[listID][MyData.intListIndex[listID]] = *( int* )param ;
-          MyData.PList[listID][MyData.PListIndex[listID]] = MyData.intList[listID] + MyData.intListIndex[listID]++ ;
-     }
-     MyData.PListType[listID][MyData.PListIndex[listID]++] = type ;
-     if( MyData.PListIndex[listID] == MAX_LIST_SIZE ){
-          logSisters(
-                    "No more add to PList.",
-                    ERROR,
-                    Kotone_ID
-                    ) ;
-          return ;
-     }
-
-	logSisters(
-			"fnAppendPList()",
-			ATTEND,
-			Kotone_ID
-			) ;
+     MyData.plist[listID][MyData.index[listID]] = p ;
+     MyData.index[listID]++ ;
 
      return ;
 }
 
-char _fnGetPListAsChr( int listID, int pos ){
+//------------------------
+// :[ NAME ]:
+//     fnAdd_c
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+void _fnAddList_c( int listID, char c ){
 
-     MyData.valueChr = *( char* )MyData.PList[listID][pos] ;
+     MyData.clist[listID][MyData.index[listID]] = c ;
+     MyData.index[listID]++ ;
 
-	logSisters(
-			"fnGetPListAsChr()",
-			ATTEND,
-			Kotone_ID
-			) ;
-
-     return MyData.valueChr ;
+     return ;
 }
 
-char* _fnGetPListAsStr( int listID, int pos ){
+//------------------------
+// :[ NAME ]:
+//     fnAdd_s
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+void _fnAddList_s( int listID, string s ){
 
-     strcpy( MyData.valueStr, ( char* )MyData.PList[listID][pos] ) ;
+     MyData.slist[listID][MyData.index[listID]] = s ;
+     MyData.index[listID]++ ;
 
-	logSisters(
-			"fnGetPListAsStr()",
-			ATTEND,
-			Kotone_ID
-			) ;
-
-     return MyData.valueStr ;
+     return ;
 }
 
-int _fnGetPListAsInt( int listID, int pos ){
+//------------------------
+// :[ NAME ]:
+//     fnAdd_i
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+void _fnAddList_i( int listID, int i ){
 
-     MyData.valueInt = *( int* )MyData.PList[listID][pos] ;
+     MyData.ilist[listID][MyData.index[listID]] = i ;
+     MyData.index[listID]++ ;
 
-	logSisters(
-			"fnGetPListAsInt()",
-			ATTEND,
-			Kotone_ID
-			) ;
-
-     return MyData.valueInt ;
-}
-
-bool _isNotExistList( int listID, char type ){
-     if( MyData.existList[listID] == type ){
-          return FALSE ;
-     }
-
-     return TRUE ;
-}
-
-bool _isNextPListTypeChr( int listID ){
-     if( MyData.PListType[listID][MyData.PListIndexAccess[listID]] == TYPE_CHR ){
-          MyData.PListIndexAccess[listID]++ ;
-          return TRUE ;
-     }
-
-     return FALSE ;
-}
-
-bool _isNextPListTypeStr( int listID ){
-     if( MyData.PListType[listID][MyData.PListIndexAccess[listID]] == TYPE_STR ){
-          MyData.PListIndexAccess[listID]++ ;
-          return TRUE ;
-     }
-
-     return FALSE ;
-}
-
-bool _isNextPListTypeInt( int listID ){
-     if( MyData.PListType[listID][MyData.PListIndexAccess[listID]] == TYPE_INT ){
-          MyData.PListIndexAccess[listID]++ ;
-          return TRUE ;
-     }
-
-     return FALSE ;
+     return ;
 }
--- dir_Girls/Kotone/Kotone.h
+++ dir_Girls/Kotone/Kotone.h
@@ -2,10 +2,6 @@
 #define _KOTONE_
 
 
-//--< Sisters ID >--//
-#define Kotone_ID 6
-
-
 //--< Use Headers >--//
 #include "stdio.h"
 #include "string.h"
@@ -15,90 +11,48 @@
 #include "../dir_Specifications/siscmn.h"
 #include "../dir_Specifications/sisdef.h"
 
-#define LIST_TOP 0
-
 
 //--< Prototype Declarations >--//
-int mfnKotone( int, char*[] ) ;
-
-int _fnCreateList( char, char ) ;
-void _fnAppendPList( int, void*, char ) ;
-char _fnGetPListAsChr( int, int ) ;
-char* _fnGetPListAsStr( int, int ) ;
-int _fnGetPListAsInt( int, int ) ;
-
-bool _isNotExistList( int, char ) ;
-bool _isNextPListTypeChr( int ) ;
-bool _isNextPListTypeStr( int ) ;
-bool _isNextPListTypeInt( int ) ;
+int _fnCreateList( mode ) ;
 
 
 /* <] - Kotone - [> */
 typedef struct _kotone_functions{
-     int ( *fnCreateList )( char, char ) ;
-     void ( *fnAppendPList )( int, void*, char ) ;
-     char ( *fnGetPListAsChr )( int, int ) ;
-     char* ( *fnGetPListAsStr )( int, int ) ;
-     int ( *fnGetPListAsInt )( int, int ) ;
-
-     bool ( *isNextPListTypeChr )( int ) ;
-     bool ( *isNextPListTypeStr )( int ) ;
-     bool ( *isNextPListTypeInt )( int ) ;
+     int ( *fnCreateList )( mode ) ;
 }KotoneFunctions ;
 
 #define BIRTH_KOTONE {\
-     _fnCreateList,\
-     _fnAppendPList,\
-     _fnGetPListAsChr,\
-     _fnGetPListAsStr,\
-     _fnGetPListAsInt,\
-     \
-     _isNextPListTypeChr,\
-     _isNextPListTypeStr,\
-     _isNextPListTypeInt\
+     _fnCreateList\
 }
 
-static KotoneFunctions _Kotone = BIRTH_KOTONE ;
-static KotoneFunctions* Kotone = &_Kotone ;
+static KotoneFunctions Kotone ;
+
+KotoneFunctions callKotone( void ) ;
+
 
 //--< Mydata Declaration >--//
 typedef struct myDataKotone{
-     int listSeq ;
-     char existList[MAX_LIST_COUNT] ;
-     char valueChr ;
-     char valueStr[MAX_LENGTH] ;
-     int valueInt ;
-     char chrList[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
-     char strList[MAX_LIST_COUNT][MAX_LIST_SIZE][MAX_LENGTH] ;
-     int intList[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
-     void* PList[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
-     char PListType[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
-     int PListIndex[MAX_LIST_COUNT] ;
-     int chrListIndex[MAX_LIST_COUNT] ;
-     int strListIndex[MAX_LIST_COUNT] ;
-     int intListIndex[MAX_LIST_COUNT] ;
-     int PListIndexAccess[MAX_LIST_COUNT] ;
+     int seq ;
+     pointer plist[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
+     char clist[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
+     string slist[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
+     int ilist[MAX_LIST_COUNT][MAX_LIST_SIZE] ;
+     int index[MAX_LIST_COUNT * 4] ;
 }_MyDataKotone ;
 
 #define INIT_KOTONE_MYDATA {\
-     0,\
-     { NL },\
-	NL,\
-	{ NL },\
-	0,\
-	{ { NL } },\
-	{ { { NL } } },\
-	{ { NL } },\
-	{ { NULL } },\
-	{ { NL } },\
-	{ LIST_TOP },\
-	{ LIST_TOP },\
-	{ LIST_TOP },\
-	{ LIST_TOP },\
-	{ LIST_TOP }\
+	1,\
+     { { NULL } },\
+     { { NL } },\
+     { { NULL } },\
+     { { NL } },\
+     { 0 }\
 }
 
 
 //--< Free Definitions >--//
+#define TMP_LIST 0
+#define LIST_TOP 0
+
 
 #endif
