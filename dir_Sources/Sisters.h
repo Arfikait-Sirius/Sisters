99f21d398188e7f33cf703bc0e0186084f98bbb9
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2020-07-18 12:44:43 +0900

    Create sources directory.

---
+++ dir_Sources/Sisters.h
@@ -0,0 +1,15 @@
+#ifndef _SISTERS_
+#define _SISTERS_
+
+#include "../dir_Specifications/siscmn.h"
+#include "../dir_Runtimefiles/sisset.h"
+
+#include "../dir_Girls/Chloe/Chloe.h"
+#include "../dir_Girls/Dorothy/Dorothy.h"
+#include "../dir_Girls/Emily/Emily.h"
+#include "../dir_Girls/Grace/Grace.h"
+#include "../dir_Girls/Louise/Louise.h"
+#include "../dir_Girls/Sylvia/Sylvia.h"
+#include "../dir_Girls/Veera/Veera.h"
+
+#endif
