78c414478a826f37612c2eb8174ea31de8c57a8a
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2020-12-05 00:29:10 +0900

    Reset repository.

---
+++ dir_Examinations/Exams/forSylvia.h
@@ -0,0 +1,12 @@
+#ifndef _FOR_SYLVIA_
+#define _FOR_SYLVIA_
+
+#include "../../dir_Sources/Sisters.h"
+
+#include "../ExamCommon.h"
+
+void examForSylvia( void ) ;
+
+static void examFnCalcStrExpression( void ) ;
+
+#endif
--- dir_Runtimefiles/sisset.h
+++ dir_Runtimefiles/sisset.h
@@ -5,35 +5,47 @@
 // [ COMMON ]
 #define LOG_SETUP 1
 
-#define ATD 0
-#define ERR 1
-#define FTL 2
-#define INF 3
-#define MSG 4
+#define MSG 0
+#define INF 1
+#define ERR 2
+#define FTL 3
+
+#define TMP_LIST 0
+#define TMP_STACK 0
+#define TMP_QUEUE 0
+#define TMP_MAP 0
+
+#define LIST_TOP 0
+#define STACK_TOP 0
+
+#define MAX_LIST_COUNT 32
+#define MAX_STACK_COUNT 32
+
+#define MAX_LIST_SIZE 256
+#define MAX_STACK_SIZE 256
 
 
 // Dorothy
-#define MAX_FILE_COUNT 16
-#define READ_BUFSIZE 1024
+#define DOROTHY_MAX_FILE_COUNT 16
+#define DOROTHY_READ_BUFSIZE 1024
 
 
 // Louise
-#define MAX_CPY_SIZE 512
+#define LOUISE_MAX_CPY_SIZE 512
 
 
-// Kotone
-#define MAX_LIST_COUNT 16
-#define MAX_LIST_SIZE 128
-
-
-// Sylvia
-#define OPERATOR_STACK_BUFSIZE 64
-#define NUMBER_STACK_BUFSIZE 64
+// Rachel
+#define RACHEL_MAX_SCHEMA_COUNT 8192
+#define RACHEL_MAX_NEW_SCHEMA_COUNT 1024
 
 
 // Grace
-#define GRACE_PATH "/Users/Sirius/SiriusFramework/Sisters/dir_Runtimefiles/Grace/Source/%s.grace"
-#define GRACE_PROPERTY "/Users/Sirius/SiriusFramework/Sisters/dir_Runtimefiles/Grace/Property/%s_Property.grace"
+#define GRACE_PATH "../dir_Rooms/Grace/Source/%s.grace"
+#define GRACE_PROPERTY "../dir_Rooms/Grace/Property/%s.property.grace"
+
+
+// Veera
+#define BUF_SIZE_DATE 16
 
 
 #endif
