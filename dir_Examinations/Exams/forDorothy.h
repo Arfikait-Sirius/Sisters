0ecba64accfe5c66c2ef6d023dea2b487117d43e
Sirius Verl Arfikait <63843504+Arfikait-Sirius@users.noreply.github.com>
2021-03-01 23:33:46 +0900

    Delete Examinations.

--- dir_Examinations/Exams/forDorothy.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FOR_DOROTHY_
-#define _FOR_DOROTHY_
-
-#include "../../dir_Sources/Sisters.h"
-
-void examForDorothy( void ) ;
-
-
-#endif
--- dir_Examinations/SistersExams.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SISTERS_EXAMS_
-#define _SISTERS_EXAMS_
-
-#include "./Exams/forLouise.h"
-#include "./Exams/forSylvia.h"
-#include "./Exams/forVeera.h"
-
-
-#endif
