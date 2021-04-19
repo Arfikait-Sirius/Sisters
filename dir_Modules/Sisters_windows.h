8ec344443ac55ea4238070a4848c9f810c973c7e
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2021-04-19 21:30:29 +0900

    Update dressup.

---
+++ dir_Modules/Sisters_windows.h
@@ -0,0 +1,13 @@
+#ifndef _SISTERS_
+#define _SISTERS_
+
+#include "./sisters/skillsheets/Chloe.h"
+#include "./sisters/skillsheets/Dorothy.h"
+#include "./sisters/skillsheets/Emily.h"
+#include "./sisters/skillsheets/Louise.h"
+#include "./sisters/skillsheets/Rachel.h"
+#include "./sisters/skillsheets/Sylvia.h"
+#include "./sisters/skillsheets/Tina.h"
+#include "./sisters/skillsheets/Veera.h"
+
+#endif
