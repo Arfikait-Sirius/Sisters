df128648e78df01565ae20ac5fa4891ae6113cfc
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2021-04-24 21:31:52 +0900

    Retake Louise. - Make up.

--- dir_Specifications/siscmn.c
+++ dir_Specifications/siscmn.c
@@ -1,10 +1,8 @@
 #include "siscmn.h"
 
 int chao( void ){
-     printf( "Finish!!\n" ) ;
 
      if( _LouiselvCalled() ){
-          printf( "Leave Louise.\n" ) ;
           _LouiselvFree() ;
      }
 
