1c187b8bd8c04a0da4bb99251d7229a77ed21bc4
Arfikait Sirius <arfikait.sirius@gmail.com>
2025-05-11 02:58:48 +0900

    Retake Louise. - Make up.

---
+++ apply.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+git switch Introduce
+git pull origin Live
+
+git switch Learn
+git pull origin Live
+
+git switch Retake
+git pull origin Live
+
+git switch Live
+git pull origin Live
+
+echo "Completed!"
